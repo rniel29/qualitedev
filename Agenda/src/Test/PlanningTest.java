29f6d963f6f281657c2218cbcd487374217c9700
Ronan Niel <ronannielmtl@gmail.com>
2025-04-07 16:52:12 +0200

    test

---
+++ Agenda/src/Test/PlanningTest.java
@@ -0,0 +1,21 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanningTest {
+
+    @org.junit.jupiter.api.Test
+    void ajout() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getReservation() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void plusAncienneReservation() {
+
+    }
+}
